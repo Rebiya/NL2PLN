275b2222368ba95fb9ae710524e9657189d641f3
Roman Treutlein <roman@trueagi.io>
2025-12-03 07:22:00 +0100

    Simplify (remove rules)

--- src/nl2pln.py
+++ src/nl2pln.py
@@ -1,6 +1,9 @@
+import os
 import dspy
 import json
 import logging
+import mlflow
+import traceback
 
 logger = logging.getLogger(__name__)
 
@@ -9,241 +12,113 @@ from textwrap import dedent
 from cleanPLN import checkStmt, checkQuery, checkImpl, balance_parentheses
 from mm2chainer import MorkHandler
 
-class RuleGenerationSignature(dspy.Signature):
-    """
-    You task is to generate a list of rules that are required to answer the target query given the input statements.
-    Rules have the form:
-    (: rule_name (Implication (Predicate1 $x) (Predicate2 $x)) (STV 1.0 1.0))
-    or with conjunctions/disjunctions:
-    (: rule_name (Implication (And (Predicate1 $x) (Predicate2 $x)) (Or (Predicate3 $x) (Predicate4 $x))) (STV 1.0 1.0))
-
-    Example:
-    input_statement:
-    (: mas_is_dog (Dog max) (STV 1.0 1.0))
-    target_query:
-    (: $max_is_what_animal (Animal max $type) $tv)
-
-    required_rules:
-    (: dog_to_animal_type (Implication (Dog $d) (Animal $d dog)) (STV 1.0 1.0))
-
-    Make sure that the rules generated are logically valid. Do not just generate a rule from input to output if it makes no sense.
-    If it's not possilbe to reach the target query using generaly valid rules then return nothing
-
-    Example of rephrasing:
-    (: take_to_taking (Implication (Take $x $y) (Taking $x $y)) (STV 1.0 1.0))
-
-    Example of negation:
-    (: take_to_not_leave (Implication (Take $x $y) (Not (Leave $x $y))) (STV 1.0 1.0))
-    (: take_to_not_leave (Implication (Take $x $y) (Leave $x $y)) (STV 0.0 1.0))
-    """
-    input_statements : List[str] = dspy.InputField(desc="The input statements to be converted to PLN")
-    target_query : str = dspy.InputField(desc="The target query to be answered")
-
-    required_rules : List[str] = dspy.OutputField(desc="The required rules to answer the target query")
-
-class NLToPLNSignature(dspy.Signature):
-    """
-    Convert the input english to PLN Light.
-    Format:
-    (: prf statment truth_value)
-
-    prf should be either a specific name for this proof or a variable $prf for queries
-    statment can range from a simple (Predicate obj) to complex (Implicaiton (And (Predicate1 $x) (Predicate2 $x)) (Or (Predicate3 $x) (Predicate4 $x)))
-    or anything in between. For quereis we can also have varaiables in the place of any other element like ($pred obj) or ($connection (Predicate1 $x) (Predicate2 $x))
-
-    truth_value have the shape of (STV strenght confidence) where strenght and confidence are floats between 0 and 1.
-    again for quereis they should usualy just be a variable $tv
-
-    Example:
-    English: Max is a Dog
-    PLN Light: (: max_is_dog (Dog max) (STV 1.0 1.0))
-
-    English: Is Max a Dog?
-    PLN Light: (: $max_is_dog_prf (Dog max) $tv)
-
-    English: All dogs chase a cat.
-    PLN Light: (: dogs_chase_cat (Implication (Dog $dog) (And (Cat $cat) (Chase $dog $cat))) (STV 1.0 1.0)) 
-    Note: Unbound varaibles in the conclusion of an Implication like above are automatically excistantially quantified.
-          Universal Quantification/Forall is represented by Implication
-
-    Make sure to use a granual representation so it will be easy to answer a variaty of question about the sentence:
-    English: The Dog has brown fur.
-    BAD: 
-    (: dog_has_brown_fur (Has dog brown_fur) (STV 1.0 1.0))
-
-    GOOD:
-    (: dog (Dog dog) (STV 1.0 1.0))
-    (: has (Has dog fur) (STV 1.0 1.0))
-    (: fur (Fur fur) (STV 1.0 1.0))
-    (: brown_fur (Color brown fur) (STV 1.0 1.0))
-
-    When translating natural language to logic, certain words carry meanings that differ from their literal interpretation due to common usage. These words often express quantifiers, modalities, or degrees of frequency/intensity that require careful handling to capture their intended meaning.
-    These words are stuff like "always", "never", "frequently", "sometimes", "often", "rarely", "very", "extremely", "a lot", "a little", "some", "many", "few", "fewer", "more", "most"
-    when possilbe enocde their meing in the TruthValue.
-    For example:
-    English: The dog is sometimes happy.
-    (: dog (Dog dog) (STV 1.0 1.0))
-    (: dog_happy (Happy dog) (STV 0.1 0.7))
-    So probablity of 0.1 with condifdenc 0.7 so basically the do is happy 10% of the time.
-
-    Or alternatively: 
-    English: The dog is always running.
-    (: dog (Dog dog) (STV 1.0 1.0))
-    (: dog_running (Running dog) (STV 0.5 1.0))
-    The dog can't literally always run all the time.
-
-    Take care that for question the prf and the tv should be variables (starting with $)
-
-    The above examples are just that examples of a possible solution. Before you generate your answer. Pick a probabilty between 0 and 1.
-    That describing how likely your answer is.
-    """
-    english : str = dspy.InputField(desc="The input sentences to be converted to PLN Light")
-
-    plnl : List[str] = dspy.OutputField(desc="The converted PLN Light")
-
 class NL2PLNModule(dspy.Module):
-    def __init__(self, model: str = "openai/gpt-4o"):
-        self.model : str = model
-        self.nl2pln : dspy.Module = dspy.ChainOfThought(NLToPLNSignature)
-        self.rulegen : dspy.Module = dspy.ChainOfThought(RuleGenerationSignature)
+    def __init__(self):
+        self.nl2pln : dspy.Module = dspy.ChainOfThought("sentences: list[str] , context: list[str] -> pln_light: list[str]")
 
     def forward(self, sentences : List[str], queries: List[dict]):
-        stmts = self.nl2pln(english=sentences).plnl
+        stmts = self.nl2pln(sentences=sentences,context=[]).pln_light
 
         queries_pln = []
         for q in queries:
-            pln_q = self.nl2pln(english=q['question']).plnl
-            #if pln_q is None or len(pln_q) != 1:
-            #raise Exception("NL2PLN returned more than one query: " + str(pln_q))
-            rules = self.rulegen(input_statements=stmts, target_query=pln_q[0]).required_rules
-            queries_pln.append({'query': pln_q[0],'rules': rules})
+            pln_q = self.nl2pln(sentences=[q['question']],context=stmts)
+            queries_pln.append(pln_q.pln_light)
 
         return dspy.Prediction(statements=stmts, queries=queries_pln)
 
-def extract_rule_name(rule: str) -> str:
-    """Extract the rule name from a PLN rule string, e.g., 'extract_any_from_and' from '(: extract_any_from_and ...'."""
-    parts = rule.strip().split()
-    if len(parts) >= 2 and parts[0] == '(:':
-        return parts[1]
-    return ""
+pln_spec = """
+A pln light statment has the following form:
+(: PRF TYPE TRUTH_VALUE)
+PRF can be either a specific name or a varaible $prf in the case of queries.
+TYPE can be one of:
+    A Predicate applied to on or more objects (Predicate x y)
+    Which can be combined using And Or Implication.
+        Example: (And (Predicate1 x) (Predicate2 x))
+    Statments should have variables $var only inside Implications.
+    Variables in the premises are universally quantified.
+    Variables that appear only in the coclusion are existentially quantified.
+        Example: (Implication (Predicate1 $x $y) (And (Predicate2 $y $z) (Predicate3 $z))) [$x $y are universally quantified, $z is existentially quantified]
+    Queries can have variables at any location that a Predicate or Object could appear.
+        Example: ($pred x) / (Pred $x)
+TRUTH_VALUE can be either (STV strength confidence) with strenght and confidence between 0 and 1
+            or a variable $tv in the case of queries.
+"""
 
-class RuleValidationSignature(dspy.Signature):
-    """
-    Validate a rule to make sure it is logically valid.
-    These rules are not knowledge but are generated to map between differnt represenations.
-    So they should be obvioulsy true in any context.
-    This includes renaming/repharsing things so:
-    (: take_to_taking (Implication (Take $x $y) (Taking $x $y)) (STV 1.0 1.0))
-    is fine also:
-    (: take_to_not_leave (Implication (Take $x $y) (Not (Leave $x $y))) (STV 1.0 1.0))
-    or encoded as:
-    (: take_to_not_leave (Implication (Take $x $y) (Leave $x $y)) (STV 0.0 1.0))
-    Note that unbound $variables in the conlusion are existentially quantified.
-    Valid Example:
-    (: red_to_color (Implication (Red $x) (And (Color $x $red) (Name $red 'Red'))) (STV 1.0 1.0))
+def format_result(q, proof, comp):
+    return dedent(f"""
+        Proof: {proof}
+        for question: '{q['question']}'
+        matched expected answer: {q['expected_answer']}
+        with similarity: {comp.proof_exists_and_matches_expected_answer}
+        Reasoning: {comp.reasoning}
+    """)
 
-    Also take into consideration the Truth Value (STV strength confidence)
-    a rule with low strenght or conficence might not need as much scrutiny as one with (STV 1.0 1.0)
-    For example:
-    (: tail_white_rule (Implication (And (TailOf $animal $tail) (Color $tail $color)) (Color $animal $color)) (STV 0.2 0.5))
-    while the color of the tail does not necesaryl tell us the color of the animal. Many animals have the same color on their whole body including the tail.
-    So a 20% chance that the tail indicates the same color as the animal is acceptable.
-    """
-    rule : str = dspy.InputField(desc="The rule to be validated")
-    is_logically_valid : bool = dspy.OutputField(desc="True if the rule is logically valid, False otherwise")
-
-class ProofComparisonSignature(dspy.Signature):
-    """
-    Compare the expected answer to the found proof.
-    The proof will have the shape (: proof_path (Proofen Statment) (STV strenght confidence))
-    strenght can be considered the probabilty that the statment is true with a given confidence
-    """
-    question : str = dspy.InputField(desc="The question")
-    expected_answer : str = dspy.InputField(desc="The expected answer")
-    found_proof : str = dspy.InputField(desc="The found proof")
-    proof_matches_expected_answer : float = dspy.OutputField(desc="The similarity between the expected answer and the found proof")
+def format_hint(gold_stmts, q):
+    return dedent(f"""
+        A possible representation (inspiration only):
+        Statements: {gold_stmts}
+        Queries: {q['query']}
+    """)
 
 def difficulty_metric(gold: dspy.Example, pred: dspy.Prediction, trace=None, pred_name=None, pred_trace=None):
-    metta_handler = MorkHandler()
-    compare : dspy.Module = dspy.ChainOfThought(ProofComparisonSignature)
-    validate_rule : dspy.Module = dspy.ChainOfThought(RuleValidationSignature)
+    try: 
+        metta_handler = MorkHandler()
+        compare : dspy.Module = dspy.ChainOfThought("question, expected_answer, proof -> proof_exists_and_matches_expected_answer: float")
 
-    log = False
+        log = False
 
-    penalty = 0
-    penalty_reason = ""
+        score = 0.0
+        if pred.statements == [] or pred.statements is None:
+            return dspy.Prediction(score=score, feedback="No pln statements found")
 
-    if pred.statements == [] or pred.statements is None:
-        return dspy.Prediction(score=0.0, feedback="No pln statements found")
+        for stmt in pred.statements:
+            if checkStmt(stmt) == 0.0:
+                return dspy.Prediction(score=score, feedback=
+                    f"""The statement {stmt} did not follow the right syntax. Follow the pln light spec {pln_spec}""")
+            score += 0.001
+            metta_handler.add_atom(stmt,log=log)
+        
+        for query in pred.queries:
+            #if len(query) != 1:
+                #return dspy.Prediction(score=score, feedback="Found multiple queries where only one is expected.")
+            #score += 0.001
+            if checkQuery(query[0]) == 0.0:
+                return dspy.Prediction(score=score, feedback=
+                    f"""The query {query[0]} did not follow the right syntax. Follow the pln light spec {pln_spec}""")
+            score += 0.001
 
-    for stmt in pred.statements:
-        stmt , score = balance_parentheses(stmt)
-        if checkStmt(stmt) == 0.0:
-            return dspy.Prediction(score=0.0, feedback=
-                f"""The statement {stmt} did not follow the right syntax.
-                    it should look like (: proof_name (Predicate x) (STV strength confidence))""")
-        metta_handler.add_atom(stmt,log=log)
-    
-    for qr in pred.queries:
-        query = qr['query']
-        query , score = balance_parentheses(query)
-        if checkQuery(query) == 0.0:
-            return dspy.Prediction(score=0.0, feedback=
-             f"""The query {query} did not follow the right syntax.
-                 It should look like (: $prf (Predicate x) $tv)""")
-
-        for rule in qr['rules']:
-            rule , score = balance_parentheses(rule)
-            if checkImpl(rule) == 0.0:
-                return dspy.Prediction(score=0.0, feedback=
-                    f"""One of the pln rule:\n {rule}
-                        did not follow the right syntax.
-                        It should look like (: proof_name (Implication (PredicateA x) (PredicateB x)) (STV strength confidence))""")
-            validation = validate_rule(rule=rule)
-            if not validation.is_logically_valid:
-                return dspy.Prediction(score=0.0, feedback=
-                    f"This rule is not logicaly sound: {rule}\nReasoning: {validation.reasoning}")
-            metta_handler.add_atom(rule,log=log)
-
-    proofs = []
-    for qr in pred.queries:
-        clean_query , _ = balance_parentheses(qr['query'])
-        proofs.append(metta_handler.query(clean_query,log=log))
-
-    correct_matches = 0
-    feedback_details = []
-    # Compare each question's proof against its expected answer
-    for i, q in enumerate(gold.queries):
-        comparison = compare( question=q['question']
-                            , expected_answer=q['expected_answer']
-                            , found_proof=proofs[i])
-        correct_matches += comparison.proof_matches_expected_answer
-        feedback_details.append(dedent(f"""
-        Proof:
-            {proofs[i]}
-        for question:
-            '{q['question']}'
-        matched expected answer:
-            {q['expected_answer']}
-        with similarity: {comparison.proof_matches_expected_answer}
-        Reasoning:
-            {comparison.reasoning}"""))
-
-        if comparison.proof_matches_expected_answer < 0.7:
-            feedback_details.append(dedent(f"""
-            A possible representation to solve this problem could be (note this representation might be able to solve this specific problem but in general might be missing quite a few things, use only as inspiration):
-            Statments:
-                {gold.statements}
-            Queries:
-                {q['query']}
-            Rules:
-                {q['rules']}"""))
+        proofs = []
+        for qr in pred.queries:
+            proofs.append(metta_handler.query(qr[0],log=log))
 
 
-    total_questions = len(pred.queries)
-    score = correct_matches / total_questions if total_questions > 0 else 0.0
-    detailed_feedback = f"Score: {correct_matches}/{total_questions} questions matched. \n" + "\n".join(feedback_details)
-    return dspy.Prediction(score=score, feedback=detailed_feedback)
+        correct_matches = 0
+        feedback_details = []
+        for query, proof in zip(gold.queries, proofs):
+            result = compare(
+                question=query['question'],
+                expected_answer=query['expected_answer'],
+                proof=proof
+            )
+            match = 0.0 if result.proof_exists_and_matches_expected_answer is None else result.proof_exists_and_matches_expected_answer
+
+            correct_matches += match
+            feedback_details.append(format_result(query, proof, result))
+
+            if match < 0.7:
+                feedback_details.append(format_hint(gold.statements, query))
+
+        n = len(pred.queries)
+        score = max(correct_matches / n if n > 0 else 0.0,0.1)
+
+        return dspy.Prediction(
+            score=score,
+            feedback=f"Score: {correct_matches}/{n} questions matched. \n" + "\n".join(feedback_details)
+        )
+    except Exception as e:
+        print(pred)
+        print("Error occured in difficulty_metric:",e)
+
 
 
 def build_examples_from_file(filepath: str) -> List[dspy.Example]:
@@ -260,35 +135,37 @@ def build_examples_from_file(filepath: str) -> List[dspy.Example]:
         )
     return examples
 
+tracking_uri = os.getenv("MLFLOW_TRACKING_URI")
+if tracking_uri:
+    mlflow.set_tracking_uri(uri=tracking_uri)
+    mlflow.set_experiment("DSPy-Optimization")
+    mlflow.dspy.autolog(
+        log_compiles=True,    # Track optimization process
+        log_evals=True,       # Track evaluation results
+        log_traces_from_compile=True  # Track program traces during optimization
+    )
+
 if __name__ == '__main__':
-    #model = "openrouter/google/gemini-2.5-flash-lite"
-    #model = "openai/gpt-4o" #{'openai/gpt-4o': {'completion_tokens': 1217, 'prompt_tokens': 5139, 'total_tokens': 6356, 'completion_tokens_details': {'accepted_prediction_tokens': 0, 'audio_tokens': 0, 'reasoning_tokens': 0, 'rejected_prediction_tokens': 0, 'text_tokens': 0}, 'prompt_tokens_details': {'audio_tokens': 0, 'cached_tokens': 1536, 'text_tokens': 0, 'image_tokens': 0}}}
-    #model = "openai/o3" #{'openai/o3': {'completion_tokens': 6834, 'prompt_tokens': 5279, 'total_tokens': 12113, 'completion_tokens_details': {'accepted_prediction_tokens': 0, 'audio_tokens': 0, 'reasoning_tokens': 5376, 'rejected_prediction_tokens': 0, 'text_tokens': 0}, 'prompt_tokens_details': {'audio_tokens': 0, 'cached_tokens': 1536, 'text_tokens': 0, 'image_tokens': 0}}}
-    #model = "openai/gpt-4.1"
-    #model = "openrouter/z-ai/glm-4.6"
     #model = "openrouter/openai/gpt-5.1"
-    model = "cerebras/gpt-oss-120b"
-    #model  = "openrouter/anthropic/claude-haiku-4.5"
-    #model = "openrouter/openai/gpt-oss-120b"
-    #model = "openrouter/qwen/qwen3-235b-a22b-thinking-2507"
-    #model = "openrouter/meta-llama/llama-3.3-70b-instruct"
+    model = "openrouter/deepseek/deepseek-v3.2"
+    #model = "cerebras/gpt-oss-120b"
 
     dspy.configure(lm=dspy.LM(model,temperature=1.0, max_tokens=20000))
     dspy.settings.configure(track_usage=True)
 
-    module = NL2PLNModule(model=model)
-    module.load("programs/sample_module_optimzied.json")
+    module = NL2PLNModule()
+    module.load("programs/manual0.json")
 
-    puzzle_data = build_examples_from_file("data/sentences2.json")
+    puzzle_data = build_examples_from_file("data/sentences.json")
 
     puzzle_data = [puzzle_data[0]]
 
     score_sum = 0
     for puzzle in puzzle_data:
         res = module(sentences=puzzle.sentences, queries=puzzle.queries)
-        metric = difficulty_metric(puzzle, res)
         print("------------------------------------------------------------------------------------------------------------------------------")
         print(res)
+        metric = difficulty_metric(puzzle, res)
         print("------------------------------------------------------------------------------------------------------------------------------")
         print(metric.score)
         print(metric.feedback)
